034293f0bd9fe10b45187b787a6b12911e809afd
Eumericano <opwer032@naver.com>
2022-04-28 14:46:25 +0900

    learn: Set span style in component

--- src/App.js
+++ src/App.js
@@ -2,48 +2,26 @@ import styled from "styled-components";
 
 function App() {
   return (
-    <Father>
-      <Box bgColor="teal" />
-      <Circle bgColor="brown" />
-
-      <Button>log in</Button>
-      {/* 버튼을 a링크처럼 사용하려할 때  */}
-      <Button as="a" href="/">
-        log in
-      </Button>
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-    </Father>
+    <Wrapper>
+      <Box>
+        <span>hi</span>
+      </Box>
+    </Wrapper>
   );
 }
 
 export default App;
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
-  flex-direction: column;
 `;
 
 const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
   width: 200px;
   height: 200px;
-`;
+  background-color: red;
 
-const Circle = styled(Box)`
-  border-radius: 100px;
-`;
-
-const Button = styled.button`
-  background-color: tomato;
-  color: white;
-  border: 0;
-  border-radius: 50%;
-`;
-
-// 모든 컴포넌트에 html 속성 추가하는 법
-const Input = styled.input.attrs({ required: true, maxLength: 20 })`
-  background-color: "tomato";
+  span {
+    color: white;
+  }
 `;
