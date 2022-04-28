554273a12ca370259e99c53a337ea65c6d292561
Eumericano <opwer032@naver.com>
2022-04-28 14:57:35 +0900

    feat: Set theme provider

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 function App() {
   return (
     <Wrapper>
+      <Title>타이틀</Title>
       <Box>
         <span>hi</span>
       </Box>
@@ -13,7 +14,15 @@ function App() {
 export default App;
 
 const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+
   display: flex;
+  background-color: ${(props) => props.theme.backgroundColor};
+`;
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
 `;
 
 const Box = styled.div`
